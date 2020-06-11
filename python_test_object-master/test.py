8590974e322b522a33decac6408fb3464fa7dc1c
Scott Deng <38242567+Kurokawashiro@users.noreply.github.com>
2020-06-11 10:18:09 +0900

    this is python test
    valid until July

---
+++ python_test_object-master/test.py
@@ -0,0 +1,28 @@
+
+def fizzbuzz(file_str = './input.txt'):
+    f = open(file_str)
+    res = []
+    for x in f:
+        res.append(x)
+    f.close()
+
+    seq = []
+    for i in res:
+        if ':' in i:
+            a = i.split(':')
+            b = a[1].split('\n')
+            seq.append((int(a[0]),b[0]))
+        else:
+            m = int(i.split('\n')[0])
+
+    seq.sort()
+
+    res_str = ""
+    for t in seq:
+        if m%t[0]==0:
+            res_str+=t[1]
+    if res_str=="":
+        res_str = m
+
+
+    return res_str
